cd5dad586109c8fda914bcf2a9c7be088e10fcbd
Jonathan García <jona03g97@gmail.com>
2022-04-23 21:50:02 -0600

    Delete: hook

--- src/App.js
+++ src/App.js
@@ -1,103 +1,38 @@
+import getCharacters from './getCharacters'
+import { useState, useEffect, useRef, useCallback } from 'react'
 import Characters from './components/Characters'
-const characters = [
-  {
-    id: 1,
-    name: 'Rick Sanchez',
-    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
-    origin: {
-      name: 'Citadel of Ricks'
-    },
-    location: {
-      name: 'Citadel of Ricks'
-    }
-  },
-  {
-    id: 2,
-    name: 'Rick Sanchez',
-    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
-    origin: {
-      name: 'Citadel of Ricks'
-    },
-    location: {
-      name: 'Citadel of Ricks'
-    }
-  },
-  {
-    id: 3,
-    name: 'Rick Sanchez',
-    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
-    origin: {
-      name: 'Citadel of Ricks'
-    },
-    location: {
-      name: 'Citadel of Ricks'
-    }
-  },
-  {
-    id: 4,
-    name: 'Rick Sanchez',
-    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
-    origin: {
-      name: 'Citadel of Ricks'
-    },
-    location: {
-      name: 'Citadel of Ricks'
-    }
-  },
-  {
-    id: 5,
-    name: 'Rick Sanchez',
-    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
-    origin: {
-      name: 'Citadel of Ricks'
-    },
-    location: {
-      name: 'Citadel of Ricks'
-    }
-  },
-  {
-    id: 6,
-    name: 'Rick Sanchez',
-    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
-    origin: {
-      name: 'Citadel of Ricks'
-    },
-    location: {
-      name: 'Citadel of Ricks'
-    }
-  },
-  {
-    id: 7,
-    name: 'Rick Sanchez',
-    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
-    origin: {
-      name: 'Citadel of Ricks'
-    },
-    location: {
-      name: 'Citadel of Ricks'
-    }
-  },
-  {
-    id: 8,
-    name: 'Rick Sanchez',
-    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
-    origin: {
-      name: 'Citadel of Ricks'
-    },
-    location: {
-      name: 'Citadel of Ricks'
-    }
-  }
-]
+import useVisible from './hooks/useVisible'
 
 function App() {
+  const [characters, setCharacters] = useState([])
+  const [page, setPage] = useState(1)
+  const visorRef = useRef()
+  const { visible } = useVisible({ visorRef })
+
+  const changePage = useCallback(() => setPage((prev) => prev + 1), [])
+
+  useEffect(() => {
+    if (visible) {
+      changePage()
+    }
+  }, [visible, changePage])
+
+  const gettingCharacters = async ({ page }) => {
+    const results = await getCharacters({ page })
+    setCharacters((prev) => prev.concat(results))
+  }
+
+  useEffect(() => {
+    gettingCharacters({ page })
+  }, [page])
+
   return (
     <div className='App'>
       <header className='App-header'>
         <h1>Prueba de intersection observer</h1>
       </header>
       <Characters characters={characters} />
-      <div className='visor'></div>
+      <div className='visor' ref={visorRef}></div>
     </div>
   )
 }
